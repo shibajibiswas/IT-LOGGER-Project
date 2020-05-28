3b44a54e239df236855b5badd8a92a0e3bd2557d
Shibaji Biswas <shibaji005@gmail.com>
2020-05-28 23:26:51 +0530

    This is IT Logger Project for registering technician jobs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, Fragment} from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import Addbtn from './components/layout/Addbtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
+import { Provider } from 'react-redux';
+import store from './store';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  useEffect(()=>{
+    M.AutoInit();
+  });
+  return ( 
+    <Provider store={store}>
+  <Fragment>
+    <SearchBar/>
+    <div className='container'>
+      <Addbtn/>
+      <AddLogModal/>
+      <EditLogModal/>
+      <AddTechModal/>
+      <TechListModal/>
+      <Logs/>
     </div>
+  </Fragment>
+  </Provider>
   );
 }
 
